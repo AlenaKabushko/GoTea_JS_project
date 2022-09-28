f7b3507d6aeba617a1f997286318fe8cd85c4902
AlenaKabushko <100042777+AlenaKabushko@users.noreply.github.com>
2022-09-28 12:06:28 +0000

    Deploying to gh-pages from @ AlenaKabushko/GoTea_JS_project@b5cc7d36cdb4e6bd634d467f800df86585c3bb79 🚀

--- index.acaee7c6.js
+++
@@ -1,2 +0,0 @@
-!function(){var o;o="GoTea",console.log("I'm JS. Hello, ".concat(o,"!"))}();
-//# sourceMappingURL=index.acaee7c6.js.map
--- index.ace75974.js
+++
@@ -1,2 +0,0 @@
-var o;o="GoTea",console.log(`I'm JS. Hello, ${o}!`);
-//# sourceMappingURL=index.ace75974.js.map
