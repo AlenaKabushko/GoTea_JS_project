8a0a17ade52a26baa71633607e8c0bb03a01f343
yanakhorolska <yana.khorolska@protonmail.com>
2022-09-28 14:00:46 +0200

    update fetch header

--- src/index.js
+++ src/index.js
@@ -1,2 +1,3 @@
 import './js/header';
+import './js/fetchUrl';
 
--- src/js/fetchUrl.js
+++ src/js/fetchUrl.js
@@ -6,48 +6,51 @@ const refs = {
 const API_KEY = '0ad512fb225eecaea999568cb90b6aa0';
 
 axios.defaults.baseURL = `https://api.themoviedb.org/3`;
+let page = 1;
 
-const getTrendingList = async (page = 1) => {
-  const { data } = await axios.get(
-    `/movie/popular?api_key=${API_KEY}&page=${page}`
-  );
-  return data;
-};
-getTrendingList();
+async function getTrendingList() {
+    const response = await axios.get(
+        `/trending/movie/day?api_key=${API_KEY}&page=${page}`
+    );
+    
+    console.log(response.data.results)
+    return response.data.results;
+    
+}
 
-function createFilmListMarkup(data) {
-  
-      return data
-          .map(
-              ({
-                  original_title,
-                  poster_path,
-                  overview,
-                  vote_average,
-                  id,
-                  genre_names,
-                  release_date,
-              }) => {
-                  return `<li class='films-gallery__item' key='${id}'>
+
+getTrendingList()
+
+
+
+
+
+function createFilmListMarkup(movies) {
+    
+    return movies
+        .map(
+            (movie) => {
+                return `<li class='films-gallery__item' key='${movie.id}'>
             <img
-              class='films-gallery__img'
-              src='${poster-path}'
-              alt='${original_title}'
-              width
-              loading='lazy'
+                class='films-gallery__img'
+                src='${movie.poster_path}'
+                alt='${movie.original_title}'
+                width
+                loading='lazy'
             />
-            <span class='films-gallery__rate'>${vote_average.toFixed(1)}</span>
+            <span class='films-gallery__rate'>${movie.vote_average.toFixed(1)}</span>
             <div class='films-gallery__wrap'>
-              <h2 class='films-gallery__title'>${original_title}</h2>
-              <div class='films-gallery__info'>
-                <p class='films-gallery__text'>${genre_names}</p>
-                <p class='films-gallery__age'>| ${release_date}</p>
-              </div>
+                <h2 class='films-gallery__title'>${movie.original_title}</h2>
+                <div class='films-gallery__info'>
+                <p class='films-gallery__text'>${movie.genre_names}</p>
+                <p class='films-gallery__age'>| ${movie.release_date}</p>
+                </div>
             </div>
-          </li>`}
-      )
-      .join('');
-  }
+            </li>`;
+}
+    )
+    .join('');
+}
 
-const filmsList = createFilmListMarkup(data.results);
-refs.filmsGallery.innerHTML = filmsList;
+
+refs.filmsGallery.insertAdjacentHTML('beforeend', createFilmListMarkup());
