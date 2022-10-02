06db4d924ae60bac2290d152c33a89db98f1a033
Rybailo <rybailo.orest4@gmail.com>
2022-10-02 19:40:30 +0300

    bugfix-search

--- src/js/search.js
+++ src/js/search.js
@@ -3,25 +3,31 @@ import {
   requestTypes,
   getServerData,
   renderMoviesMarkup,
+  markupList,
 } from './fetchUrl.js';
 
 import { spinnerOn, spinnerOff } from './spinner';
 import { fillGenresFiltr } from './filter';
 import { makePagination, makePaginationBtn } from './pagination';
 // --------------------------
-const searchForm = document.querySelector('.header__form-block');
+const searchBtn = document.querySelector('.header__form-button');
 const searchInput = document.querySelector('.header__form-input');
-/* const headerSearchError = document.querySelector('.visually-hidden'); */
+const headerSearchError = document.querySelector('.header__text-warning');
 
-searchForm.addEventListener('click', e => {
+searchBtn.addEventListener('click', e => {
   e.preventDefault();
-  requestData.search = `${searchInput.value}`;
-  requestData.page = 1;
-  getServerData(requestTypes.SEARCH).then(movies => {
-    renderMoviesMarkup(movies);
-    /*  if (requestData.search === []) {
-      return headerSearchError;
-    } */
-    searchInput.value = '';
-  });
+  if (searchInput.value !== '' && searchInput.value !== ' ') {
+    requestData.search = `${searchInput.value}`;
+    requestData.page = 1;
+    getServerData(requestTypes.SEARCH).then(movies => {
+      renderMoviesMarkup(movies);
+      searchInput.value = '';
+      /* if (( )) {
+        console.log('FUCK');
+      } */
+    });
+  } else {
+    return;
+  }
 });
+/* console.log(markupList); */
