5d349e8e8beb51227233f9921089f0efee201290
Konstantyn <k.buhantsev@gmail.com>
2022-10-03 22:39:49 +0300

    search js

--- src/js/search.js
+++ src/js/search.js
@@ -5,29 +5,27 @@ import {
   renderMoviesMarkup,
   markupList,
 } from './fetchUrl.js';
-
 import { spinnerOn, spinnerOff } from './spinner';
 import { fillGenresFiltr } from './filter';
-import { makePagination, makePaginationBtn } from './pagination';
-// --------------------------
+import { PaginationInstnce } from '../index';
+
 const searchBtn = document.querySelector('.header__form-button');
 const searchInput = document.querySelector('.header__form-input');
 const headerSearchError = document.querySelector('.header__text-warning');
 
-searchBtn.addEventListener('click', e => {
+searchBtn.addEventListener('click', onBtnSearcClick);
+
+function onBtnSearcClick(e) {
   e.preventDefault();
   if (searchInput.value !== '' && searchInput.value !== ' ') {
     requestData.search = `${searchInput.value}`;
     requestData.page = 1;
     getServerData(requestTypes.SEARCH).then(movies => {
+      PaginationInstnce.currentPage = 1;
+      PaginationInstnce.setTotalPages(Math.round(movies.total_pages / 20));
+
       renderMoviesMarkup(movies);
       searchInput.value = '';
-      /* if (( )) {
-        console.log('FUCK');
-      } */
     });
-  } else {
-    return;
   }
-});
-/* console.log(markupList); */
+}
